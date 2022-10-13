7a6a79a1a9e97343fbd54672a3403894a1664259
Gael <gael.francoise@gmail.com>
2022-10-13 10:01:22 +0200

    removed db creation line

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, ForeignKey('blog_posts.id'))
     post = relationship('BlogPost', back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
